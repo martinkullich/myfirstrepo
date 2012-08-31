9ec015fe63a99e5897a1b94fec2ca5eaac929111
mkullich <mkullch@hotmail.com>
2012-08-31 21:13:26 +0100

    Take 3

---
+++ ReconnectBugP/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using SignalR.Client;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(200);
+
+            const string serverUrl = "http://localhost:5000/echo/";
+            var connection = new Connection(serverUrl);
+
+            connection.StateChanged += OnConnectionStateChanged;
+
+            try
+            {
+                connection.Start().Wait();
+            }
+            catch(AggregateException ex)
+            {
+                throw ex.Flatten();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void OnConnectionStateChanged(StateChange obj)
+        {
+            Console.WriteLine(obj.NewState.ToString());
+        }
+    }
+}
---
+++ ReconnectBugP/Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using SignalR;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.Listeners.Add(new ConsoleTraceListener());
+            Debug.AutoFlush = true;
+
+            const string url = "http://*:5000/";
+            var server = new SignalR.Hosting.Self.Server(url);
+            server.Configuration.DisconnectTimeout = TimeSpan.Zero;
+
+            // Map connections
+            server.MapConnection<MyConnection>("/echo")
+                  .MapHubs();
+
+            server.Start();
+
+            Console.WriteLine("Server running on {0}", url);
+            Console.ReadKey();
+        }
+    }
+
+    internal class MyConnection : PersistentConnection
+    {
+        protected override System.Threading.Tasks.Task OnConnectedAsync(IRequest request, string connectionId)
+        {
+            // Put the breakpoint here.  Once it has been hit once, clear it and hit F5
+            return base.OnConnectedAsync(request, connectionId);
+        }
+    }
+}
